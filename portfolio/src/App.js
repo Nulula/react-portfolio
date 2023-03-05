8e42a92e75c144dcfb3a5bb442a2447643c963e6
Nulula <nulula@gmail.com>
2023-03-05 15:28:44 +0000

    react router setup finished

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import { Routes,Route } from 'react-router-DOM';
 
 function App() {
   return (
